db6bb3c9aa966c277044edaf77d9bf3145599224
ewertonvsilva <ewerton.vasconcelos@poli.ufrj.br>
2023-07-24 16:04:13 +0000

    add installer script

--- notes/installer.py
+++ notes/installer.py
@@ -1,51 +1,108 @@
+import shutil
 import json
+import errno
 import os
+import argparse
+
+
+def copy_item(source, destination):
+    if os.path.isfile(source):
+        # If the source is a file, use copy_file
+        copy_file(source, destination)
+        return "File"
+    elif os.path.isdir(source):
+        # If the source is a directory, use copy_tree
+        copy_tree(source, destination)
+        return "Folder"
+    
+    return "Invalid source path"
+    
+
+def copy_file(source, destination):
+    try:
+        shutil.copy2(source, destination)
+    except IOError as e:
+        if e.errno != errno.ENOENT:
+            raise
+        # try creating parent directories
+        os.makedirs(os.path.dirname(destination))
+        shutil.copy2(source, destination)
+
+def copy_tree(source, destination):
+    try:
+        shutil.rmtree(destination)  # Remove the destination folder if it exists
+    except OSError:
+        pass  # Ignore errors if the destination folder doesn't exist
+
+    shutil.copytree(source, destination)
+
+def print_pretty_table(data):
+    col_widths = [max(len(str(item)) for item in col) for col in zip(*data)]
+
+    print('-' * 30)
+    for row in data:
+        for i, item in enumerate(row):
+            print(str(item).ljust(col_widths[i] + 2), end="")
+        print()
+    print('-' * 30)
+
+
 
 def main():
-    # Ler o arquivo JSON
-    with open('instructions.json') as f:
+    parser = argparse.ArgumentParser(description='Script used to support installation procedure')
+
+    parser.add_argument('--instructions', help='json file with files instructions', required=True)
+    parser.add_argument('--backup', action='store_true', help='Make a backup from instructions')
+    parser.add_argument('--install', action='store_true', help='Make instructions installation')
+    parser.add_argument('--rollback', action='store_true', help='Make rollback from previous created backup')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Access the parsed options and values
+    with open(args.instructions) as f:
         instructions = json.load(f)
 
-    # Executar as manipulações definidas no JSON
-    for manipulation in instructions:
-        action = manipulation['action']
-        source = manipulation['source']
-        destination = manipulation['destination']
-        permissions = manipulation.get('permissions', None)
 
-        if action == 'copy':
-            copy_file(source, destination, permissions)
+    if args.backup:
+        # Execute Backup of all files:
+        backup_folder = instructions['backup_folder']
+        data = []
+        
+        print("# Backup")
 
-    print_report(instructions)
+        for manipulation in instructions['release']:
+            source = manipulation['destination']
 
-def copy_file(source, destination, permissions):
-    # Criar o arquivo de destino, se necessário
-    if not os.path.exists(destination):
-        open(destination, 'w').close()
+            try:
+                type = copy_item(source,backup_folder +"/"+source)
+                status = "Success" if type != "Invalid source path" else "Skipped"
+            except Exception as e:
+                status = repr(e)
+            
+            new_row = ["Backup", source, backup_folder +"/"+source, type, status]
+            data.append(new_row)
+        print_pretty_table(data)
 
-    # Copiar o conteúdo do arquivo de origem para o arquivo de destino
-    with open(source, 'r') as f:
-        contents = f.read()
-    with open(destination, 'w') as f:
-        f.write(contents)
+    if args.install:
+        data = []
+        print("# Installation")
 
-    # Se as permissões forem definidas, defini-las no arquivo de destino
-    if permissions is not None:
-        os.chmod(destination, permissions)
+        for manipulation in instructions['release']:
+            source = manipulation['source']
+            destination =  manipulation['destination']
 
-def print_report(instructions):
-    # Imprimir um relatório das cópias com nome dos arquivos e outros detalhes relevantes
-    print('Arquivos copiados:')
-    for manipulation in instructions:
-        action = manipulation['action']
-        source = manipulation['source']
-        destination = manipulation['destination']
-        permissions = manipulation.get('permissions', None)
+            try:
+                type = copy_item(source,destination)
+                status = "Success"
+            except Exception as e:
+                status = repr(e)
+            new_row = ["Installation", source, destination, type, status]
+            data.append(new_row)
+        print_pretty_table(data)
+    if args.rollback:
+        print('Flag is set.')
 
-        print('  Arquivo: {}'.format(source))
-        print('  Destino: {}'.format(destination))
-        if permissions is not None:
-            print('  Permissões: {}'.format(permissions))
 
 if __name__ == '__main__':
     main()
---
+++ notes/test.sh
@@ -0,0 +1,8 @@
+mkdir folder1
+touch folder1/file1.txt
+touch folder1/file2.txt
+mkdir folder2
+touch folder2/file3.txt
+touch folder2/file4.txt
+touch file1.txt
+touch file2.txt
